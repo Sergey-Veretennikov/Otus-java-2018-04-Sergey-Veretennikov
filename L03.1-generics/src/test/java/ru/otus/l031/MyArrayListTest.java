063cdfbb199a69fee950c2c519989a3e46442500
Sergey <versertraide@mail.ru>
2018-05-06 22:37:13 +0400

    L03.1-generics

--- L03.1-generics/src/test/java/ru/otus/l031/MyArrayListTest.java
+++ L03.1-generics/src/test/java/ru/otus/l031/MyArrayListTest.java
@@ -21,12 +21,8 @@ public class MyArrayListTest {
     public void collections_copy() {
         MyArrayList<String> src = new MyArrayList<>();
         Collections.addAll(src, "Item1", "Item2", "Item3");
-
-        List<String> tmp = Arrays.asList(new String[src.size()]);
-
-        MyArrayList<String> dest = new MyArrayList<>(tmp);
+        MyArrayList<String> dest = new MyArrayList<>(Arrays.asList(new String[src.size()]));
         Collections.copy(dest, src);
-
         assertArrayEquals(new String[]{"Item1", "Item2", "Item3"}, dest.toArray());
     }
 
@@ -40,7 +36,6 @@ public class MyArrayListTest {
                 return o1.compareTo(o2);
             }
         };
-
         Collections.addAll(src, "Item2", "Item3", "Item4", "Item1");
         Collections.sort(src, comparator);
         assertArrayEquals(new String[]{"Item1", "Item2", "Item3", "Item4"}, src.toArray());
