c63a2559511f481e2f50c095121fbd5e091377f4
Sergey <versertraide@mail.ru>
2018-05-12 19:46:17 +0400

    L03.1-generics

--- L03.1-generics/src/main/java/ru/otus/l031/MyArrayList.java
+++ L03.1-generics/src/main/java/ru/otus/l031/MyArrayList.java
@@ -212,4 +212,67 @@ public class MyArrayList<T> implements List<T> {
             stringBuilder.append(',').append(' ');
         }
     }
+
+    public class MyIterator<T> implements ListIterator<T> {
+        private int current = -1;
+        private List<T> list;
+
+        public MyIterator(List<T> list) {
+            this.list = list;
+        }
+
+        public MyIterator(int current, List<T> list) {
+            this.current = current;
+            this.list = list;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current < list.size() - 1;
+        }
+
+        @Override
+        public T next() {
+            if (hasNext()) current++;
+            return list.get(current);
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            return current > 0 && list.size() > 0;
+        }
+
+        @Override
+        public T previous() {
+            if (hasPrevious()) current--;
+            return list.get(current);
+        }
+
+        @Override
+        public int nextIndex() {
+            if (hasNext()) return (current + 1);
+            return -1;
+        }
+
+        @Override
+        public int previousIndex() {
+            if (hasPrevious()) return (current - 1);
+            return -1;
+        }
+
+        @Override
+        public void remove() {
+            list.remove(current);
+        }
+
+        @Override
+        public void set(T t) {
+            list.set(current, t);
+        }
+
+        @Override
+        public void add(T t) {
+            list.set(current, t);
+        }
+    }
 }
