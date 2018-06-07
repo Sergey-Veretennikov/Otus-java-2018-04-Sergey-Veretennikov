9c1763f40fa62f45781f1ce9beed8099711f7e34
Sergey <versertraide@mail.ru>
2018-06-07 17:31:47 +0400

    L07.1-ATMDepartment

---
+++ L07.1-ATMDepartment/src/main/java/ru/otus/L071/mementos/AtmMemento.java
@@ -0,0 +1,20 @@
+package ru.otus.L071.mementos;
+
+import ru.otus.L061.ATM;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AtmMemento {
+    private final List<ATM> atms = new ArrayList<>();
+
+    public void addAtmMemento(ATM atm) {
+        atms.add(atm);
+    }
+
+    public List<ATM> getAtmMemento() {
+        return atms;
+    }
+
+
+}
